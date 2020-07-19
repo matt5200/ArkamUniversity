6135b089fe7c29c77dec6e6fd8c39a12f2fae239
matthew <leematt5@uw.edu>
2020-07-19 13:14:57 -0700

    baseline project

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult ClassList()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        /*public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }*/
+
+    }
+}
